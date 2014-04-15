469399a4ba8610ddeec4956d2c647c9c7d2dea20
Ryo Nakamura <r7kamura@gmail.com>
2014-04-15 22:55:33 +0900

    bundle gem ellen-twitter

---
+++ lib/ellen/twitter.rb
@@ -0,0 +1,7 @@
+require "ellen/twitter/version"
+
+module Ellen
+  module Twitter
+    # Your code goes here...
+  end
+end
---
+++ lib/ellen/twitter/version.rb
@@ -0,0 +1,5 @@
+module Ellen
+  module Twitter
+    VERSION = "0.0.1"
+  end
+end
